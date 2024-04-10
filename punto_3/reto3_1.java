9bc7e812e246a08956a8c307a60125076438705e
Pablo1923 <pablo.loaizap@upb.edu.co>
2024-04-10 16:49:49 -0500

    correcciones

--- punto_3/reto3_1.java
+++ punto_3/reto3_1.java
@@ -27,7 +27,7 @@ class reto3_1 {
     }
 
     public static void main(String[] args) {
-        String input = "holamundo";
+        String input = "MegustaProgramarEnJava";
         System.out.println("Lo que se puso: " + input);
         System.out.println("El substring sin repetir más largo es: " + cadenaLarga(input));
     }
