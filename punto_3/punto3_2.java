da05da80b4967ff132c0851e8818ba24844cefa1
laura-152020 <104606089+laura-152020@users.noreply.github.com>
2024-04-10 16:55:18 -0500

    cambios

--- punto_3/punto3_2.java
+++ punto_3/punto3_2.java
@@ -39,7 +39,7 @@ public class punto3_2 {
     }
 
     public static void main(String[] args) {
-        String entrada = "22";
+        String entrada = "34";
         List<String> resultado = posiblesCombinaciones(entrada);
         System.out.println("Las posibles combinaciones para " + entrada + " son: " + resultado);
     }
